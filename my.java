1e0822ee296506febf2a55c51a10af74eb63d7e6
shrutisharma219 <shrutivasudev219@gamil.com>
2024-09-12 17:53:36 +0530

    nothing changed much

--- my.java
+++ my.java
@@ -5,6 +5,6 @@ public class my{
         Scanner sc = new Scanner(System.in);  // Creating a Scanner object
         String str = sc.nextLine();           // Reading input from the user
         System.out.println(str);              // Printing the input
-        sc.close();                           // Closing the Scanner object
+                                // Closing the Scanner object
     }
 }
