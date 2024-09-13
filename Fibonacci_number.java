2f8e2522d5e6af843ea1a28a5b50cd2f47c852c1
shrutisharma219 <shrutivasudev219@gmail.com>
2024-09-13 15:38:19 +0530

    check if the number is fibo or not

---
+++ Fibonacci_number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Fibonacci_number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0;
+        int b=1;
+        int c=0;
+        boolean t=false;
+        while(c<=n){
+            c=a+b;
+            a=b;
+            b=c;
+if(c==n){
+    t=true;
+    System.out.println("the number is fibo "+n);
+}
+        }
+        if(t==false){
+            System.out.println("this is not fibo "+n);
+        }
+
+    }
+    
+}
