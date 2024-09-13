c66c1daafa45ce4d523307f8231ac699634ea323
shrutisharma219 <shrutivasudev219@gmail.com>
2024-09-13 15:46:45 +0530

    count the number of occurrences of a digit

---
+++ counting_occurrences.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class counting_occurrences {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter no with occurrences");
+        int no=sc.nextInt();
+        System.out.println("enter number to count ");
+        int a=sc.nextInt();
+        int c=0;
+        int n=no;
+        while(n>0){
+            int rem=n%10;
+            if(rem==a){
+                c++;
+            }
+            n=n/10;
+            
+        }
+        System.out.println("no of occurrence of "+a+" in "+no+" is "+c);
+    }
+}
