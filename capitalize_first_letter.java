8ef0b6c4038eda71c3ff1af8ce51ddb74824ebd9
shrutisharma219 <shrutivasudev219@gmail.com>
2024-09-24 01:24:57 +0530

    captalise theb first charater of each word in string

---
+++ capitalize_first_letter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class capitalize_first_letter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String []sa=s.split(" ");
+        StringBuilder sb=new StringBuilder();
+        for(String word: sa){
+            char ch =word.charAt(0);
+            sb.append(Character.toUpperCase(ch));
+            sb.append(word.substring(1));
+            sb.append(" ");
+        }
+        String newstring=sb.toString().trim();
+        System.out.println("new string is "+newstring);
+
+
+    }
+}
