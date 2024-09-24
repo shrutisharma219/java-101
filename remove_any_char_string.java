d76c5c093a3069f0e5141961133a629381e0e5db
shrutisharma219 <shrutivasudev219@gmail.com>
2024-09-25 01:33:15 +0530

    remove any specfic char from the string

--- remove_any_char_string.java
+++ remove_any_char_string.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class remove_any_char_string {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        StringBuilder sb=new StringBuilder();
+        char ch=sc.nextLine().charAt(0);
+        //String st=Character.toString(ch);
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=ch){
+                sb.append(s.charAt(i));
+            }
+        }
+        System.out.println("new string is "+sb);
+
+    }
 }
