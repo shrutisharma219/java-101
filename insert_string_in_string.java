73d4d258cc39a9eca5b5744f4242b6700dc5e00c
shrutisharma219 <shrutivasudev219@gmail.com>
2024-09-25 23:18:54 +0530

    correct

--- insert_string_in_string.java
+++ insert_string_in_string.java
@@ -17,22 +17,6 @@ public class insert_string_in_string {
         }
         String new_string=sb.toString().trim();
         System.out.println("new string is "+new_string);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
+     
     }
 }
