6c289218778ee641fb72eb2b2ca8a88be71197d3
shrutisharma219 <shrutivasudev219@gmail.com>
2024-09-27 00:48:36 +0530

    multithreading using extend threads and sleep function

--- multithreading1.java
+++ multithreading1.java
@@ -1,18 +1,26 @@
 
 class a extends Thread{
+    @Override
     public void run(){
-        for(int i=0;i<5;i++){
-            System.out.println("int the thread");
+        try {
+            for(int i=0;i<5;i++){
+                System.out.println("int the thread");
+                Thread.sleep(1000);
+            }
+
+        } catch (InterruptedException i) {
         }
+        
     }
 }
 
 public class multithreading1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         a t=new a();
         t.start();
         for(int i=0;i<5;i++){
             System.out.println("in then main function");
+            Thread.sleep(1000);
         }
     }
 }
