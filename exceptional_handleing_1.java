cc05bc09321cc6d0f55dca5f6328ae90d55108b8
shrutisharma219 <shrutivasudev219@gmail.com>
2024-09-27 10:13:58 +0530

    exceptional handling 1

---
+++ exceptional_handleing_1.java
@@ -0,0 +1,19 @@
+public class exceptional_handleing_1 {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        try {
+            int r=5/0;
+            System.out.println(a[8]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("out of bound element");
+        }
+        catch(ArithmeticException e){
+            System.out.println(e.getStackTrace());
+            System.out.println((e.getMessage()));
+            System.out.println(e);
+
+        }
+        System.out.println("after try catch ");
+    }
+    
+}
