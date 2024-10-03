2d00b6af6429c93ea797ca4d9eb7877159ed9903
shrutisharma219 <shrutivasudev219@gmail.com>
2024-10-03 23:47:38 +0530

    program

---
+++ prime_no2.java
@@ -0,0 +1,19 @@
+public class prime_no2 {
+    boolean check(int n){
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int n=1000;
+        for(int i=2;i<=n;i++){
+            
+        }
+        
+    }
+    
+}
