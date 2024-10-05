7894bbbe787a1a4909790e6b859dde07b5833953
shrutisharma219 <shrutivasudev219@gmail.com>
2024-10-05 16:35:02 +0530

    bubble sort with boolean

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -9,22 +9,28 @@ public class bubble_sort {
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
         }
-        int c=0;
+        
 
         for(int i=0;i<n;i++){
+            boolean b=false;
             for(int j=0;j<n-i-1;j++){
                 if(a[j]>a[j+1]){
                     int t=a[j];
                     a[j]=a[j+1];
                     a[j+1]=t;
-                    c++;
+                    b=true;
                 }
             }
+            if(b==false){
+                
+                break;
+            }
         }
+
         for(int i=0;i<n;i++){
             System.out.print(a[i]+" ");
         }
-        System.out.println(c);
+    
 
     }
     
