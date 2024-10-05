c8ba1e1ae4871f6e9aa5ffb2dd4e11aef0ac39ac
shrutisharma219 <shrutivasudev219@gmail.com>
2024-10-05 17:17:29 +0530

    insertion sort

---
+++ Sorting/insertion_sort.java
@@ -0,0 +1,27 @@
+package Sorting;
+import java.util.*;
+public class insertion_sort {
+    public static void main(String[] args) {
+            int n;
+            Scanner sc=new Scanner(System.in);
+            n=sc.nextInt();
+            int []a=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+            }
+
+            for(int i=1;i<n;i++){
+                int z=a[i];
+                int j=i-1;
+                while(j>=0 && a[j]>z){
+                    a[j+1]=a[j];
+                    j--;
+                }
+                a[j+1]=z;
+            }
+
+            for(int i=0;i<n;i++){
+                System.out.print(a[i]+" ");
+            }      
+    }
+}
