c540334fd4c493bcb392e7dd274dc4e39bb4d396
shrutisharma219 <shrutivasudev219@gmail.com>
2024-10-08 23:25:58 +0530

    insertion sort

---
+++ player_digit_sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class player_digit_sum {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+    for(int i=0;i<n;i++){
+        int t=a[i];
+        while(t!=0){
+            
+        }
+    }
+    
+    }
+    
+}
