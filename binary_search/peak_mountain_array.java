e8332d18b3b9dbc5b6486a1d64c8a2e354f23492
shrutisharma219 <shrutivasudev219@gmail.com>
2024-10-13 23:34:35 +0530

    binary search closest greater  number

---
+++ binary_search/peak_mountain_array.java
@@ -0,0 +1,41 @@
+package binary_search;
+
+import java.util.Scanner;
+
+public class peak_mountain_array {
+static int mountain(int a[], int n){
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        if(a[mid]<=a[mid-1] && a[mid]>=a[mid+1]){
+            return mid;
+        }
+        else if(a[mid]>a[mid-1]){
+            s=mid+1;
+
+        }
+        else{
+            e=mid-1;
+        }
+
+    }
+    return 0;
+}
+
+         public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println("enter key to search ");
+        int k=sc.nextInt();
+        int ans=mountain(a,n);
+        System.out.println("ans is "+ans);
+}
+
+    
+}
